63e79c1cc9b40702b429446e50383c58fd925ec8
Bruno F. B. Matos <bruno.f.b.matos@gmail.com>
2025-07-27 22:00:10 -0300

    Control Flow
    Sessão Control Flow

---
+++ IfThenElse/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let from whom =
+    $"from {whom}"
+
+[<EntryPoint>]
+let main argv =
+    let message = (from "F#") // Call the function
+    let person = 
+        if argv.Length > 0 then 
+            argv.[0] 
+        else 
+            "Anonymous Person"
+        
+    printfn $"Hello world {message}, {person}"
+    0 // return an integer exit code
